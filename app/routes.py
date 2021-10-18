f555566a6be291ccc22eefd985ec2e9071f90725
Roslyn Melookaran <rmelookaran@gmail.com>
2021-10-17 18:35:54 -0700

    Added two endpoints

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,12 @@
 from flask import Blueprint
 
+hello_world_bp = Blueprint("hello_world", __name__)
+
+@hello_world_bp.route('/hello-world', methods=['GET'])
+def get_hello_world():
+    my_response = "This Sucks"
+    return my_response
+
+@hello_world_bp.route('/hello-world/JSON', methods=['GET'])
+def hello_world_json():
+    return {"name": "Roslyn", "Hobbies": ["Climbing", "Mtn Biking"]}, 200
