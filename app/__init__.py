6f80be2a78d48bc87c28bbf70badea5ea99fabb7
Roslyn Melookaran <rmelookaran@gmail.com>
2021-10-29 13:48:22 -0700

    updates pre heroku

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,34 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from dotenv import load_dotenv
+import os
 
+db = SQLAlchemy()
+migrate = Migrate()
+load_dotenv()
 
 def create_app(test_config=None):
     app = Flask(__name__)
 
-    from .routes import hello_world_bp
-    app.register_blueprint(hello_world_bp)
+    if not test_config:
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+            "SQLALCHEMY_DATABASE_URI")
+    else:
+        app.config["TESTING"] = True
+        app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+            "SQLALCHEMY_TEST_DATABASE_URI")
+
+    # Import models here
+    from app.models.book import Book
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    # Register Blueprints here
+    from .routes import books_bp
+    app.register_blueprint(books_bp)
+
     return app
